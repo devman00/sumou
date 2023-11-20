cafdb54d7347fe1fbc3bcdaa08be3927026a541d
devman00 <webmanway@gmail.com>
2023-11-20 13:31:40 +0100

    Redirect to Phone Verification page after user registration

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -30,7 +30,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    protected $redirectTo = RouteServiceProvider::ACCOUNT;
 
     /**
      * Create a new controller instance.
