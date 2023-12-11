6760449decb883510a32657a520d9fc4ed160347
devman00 <webmanway@gmail.com>
2023-12-11 01:46:18 +0100

    update national id column migration

--- database/migrations/2023_12_02_220634_add_national_id_to_users_table.php
+++ database/migrations/2023_12_02_220634_add_national_id_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('national_id')->unique()->after('role');
+            $table->unique('national_id', 'users_national_id_unique')->after('role');
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('national_id');
+            $table->dropUnique('users_national_id_unique');
         });
     }
 };
